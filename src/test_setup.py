511ac5414e99034b334f9c575bc00dbd641f42a5
Dylan Forde <forde.dylan@gmail.com>
2025-04-17 16:21:23 +0100

    updated exceptions

--- src/test_setup.py
+++ src/test_setup.py
@@ -6,9 +6,11 @@ import pandas as pd
 
 try:
     pd.read_csv('./data/tcga_data/coadread_tcga_pan_can_atlas_2018/data_cna.txt', delimiter=' ')
-except FileNotFoundError:
-    print("Data not copied correctly")
-except ModuleNotFoundError:
+except FileNotFoundError as e:
+    print("Data not copied correctly")   
+    print(e) 
+except ModuleNotFoundError as e2:
     print("Please use the conda environment")
-
+    print(e2)
+    
 print("worked correctly")
